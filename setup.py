25664e34cb5b899917192541c8ad03a779b33327
Nick Tyler <tylern@canisius.edu>
2020-12-21 10:56:36 -0500

    Remove publishing

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='nicks_plot_utils',
-    version='1.0.0',
+    version='1.0.0.1',
     description='A example Python package',
     url='https://github.com/tylern4/nicks_plot_utils',
     author='Nick Tyler',
