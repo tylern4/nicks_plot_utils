d7664f76b4da98e7e46ff318579d559a729c34d0
Nick Tyler <nicholas.s.tyler.4@gmail.com>
2022-06-28 10:49:33 -0700

    use static version

--- setup.py
+++ setup.py
@@ -6,17 +6,9 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
-git_version = subprocess.check_output(
-    ['git', 'rev-list', '--count', 'HEAD']).decode("utf-8")[:-1]
-
-with open("nicks_plot_utils/__version__", "r+", encoding="utf-8") as fh:
-    template = fh.read()
-    fh.seek(0)
-    version_parts = list(map(int, template.split('.')))
-    version_parts[-1] += 1
-    version_parts = list(map(str, version_parts))
-    version = ".".join(version_parts)
-    fh.write(version)
+with open("nicks_plot_utils/__version__", "r", encoding="utf-8") as fh:
+    version = fh.read()
+    print(version)
 
 
 setup(
